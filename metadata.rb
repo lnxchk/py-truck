8a73c5787aaabd39ce26bde7788a4bc14c2090d6
Mandi Walls <mandi.walls@gmail.com>
2015-06-19 11:52:05 +0100

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,10 @@
-name             'delivery-truck'
-maintainer       'Chef Delivery Team'
-maintainer_email 'delivery-team@chef.io'
+name             'py-truck'
+maintainer       'mandi walls'
+maintainer_email 'mandi@chef.io'
 license          'Apache 2.0'
-description      'Delivery build_cookbook for your cookbooks!'
+description      'Delivery build_cookbook for some generic pythons'
 
 version          '1.99.5'
-
-source_url       'https://github.com/opscode-cookbooks/delivery-truck'
-issues_url       'https://github.com/opscode-cookbooks/delivery-truck/issues'
-
-supports 'ubuntu', '>= 12.04'
-supports 'redhat', '>= 6.5'
-supports 'centos', '>= 6.5'
 
 depends 'delivery-sugar'
 depends 'python'
